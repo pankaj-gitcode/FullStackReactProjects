a864acb59569a040d28642520d5e1c158c28f6a9
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-14 10:20:51 +0530

    basic optimisation and files setups

--- textToImageGenerator_webApp/src/App.jsx
+++ textToImageGenerator_webApp/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+
+const Home = lazy(()=>import('./pages/Home'));
+const Result = lazy(()=>import('./pages/Result'));
+const BuyCredit = lazy(()=>import('./pages/BuyCredit'));
+const Navbar = lazy(()=>import('./components/Navbar'))
+
 
 export default function App(){
 
   return(<>
-    <div>
-      <h1 className='text-3xl font-semibold'>Hello</h1>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen
+     bg-gradient-to-b from-teal-100 to-orange-100 '>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/result' element={<Result/>}/>
+            <Route path='/buy' element={<BuyCredit/>}/>
+          </Routes>
+
+      </Suspense>
+
     </div>
   </>)
 }
