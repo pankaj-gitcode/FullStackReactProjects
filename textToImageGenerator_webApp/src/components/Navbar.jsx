68baf99067b4c849c32d7dc4fddec902f7928846
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-16 12:16:14 +0530

    navbar v2

--- textToImageGenerator_webApp/src/components/Navbar.jsx
+++ textToImageGenerator_webApp/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {useNavigate} from 'react-router-dom'
 export default function Navbar() {
   const logoToHomeNav = useNavigate();
   const navToBuy = useNavigate();
-  const [user, setUser] = useState('Raj');
+  const [user, setUser] = useState(1);
   const [credit, setCredit] = useState(4);
   const [name, setName] = useState('Raj');
    
@@ -23,15 +23,23 @@ export default function Navbar() {
               // ---------- USER LOGGED-IN --------
             <div className='flex items-center justify-center gap-8'>
                               {/* ------- CREDITS ------- */}
-              <div className='bg-[#D7EBFF] flex items-center justify-center gap-2 p-2 rounded-full'>
+              <div className='bg-[#D7EBFF] flex items-center justify-center gap-2 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition duration-[.5s]'>
                 <img src={assets.credit_star} alt={assets.credit_star} className='w-5' />
-                <p className='text-[15px] text-[#4A4A4A]'>Credit left: <span>{credit}</span></p>
+                <p className='text-[15px] text-[#4A4A4A] text-xs sm:text-sm font-medium'>Credit left: <span>{credit}</span></p>
               </div>
                                 {/* ------- USER NAME ------ */}
-              <div className='group relative flex items-center justify-center gap-2'>
-                <h1 className='text-[2vw]'>Hi! {name}</h1>
+                <h1 className=' max-sm:hidden pl-4'>Hi! {name}</h1>
+
+              <div className='group relative '>
                 <img src={assets.profile_icon} alt={assets.profile_icon} className='w-10 drop-shadow-[2px_1px_8px_rgba(0,0,0,0.3)]' />
-                <p className='absolute bottom-0 z-10 right-0 hidden group-hover:block cursor-pointer'>Logout</p>
+                                  {/* --------- LOGOUT BLOCK -------- */}
+                <div className='absolute z-10 pt-12 rounded top-0 right-0 hidden group-hover:block  cursor-pointer'>
+                  <ul className='list-none bg-white border rounded-md p-2 m-0 text-sm'>
+                    <li>Logout</li>
+                  </ul>
+                </div>
+
+                
               </div>
             </div>  
             :
