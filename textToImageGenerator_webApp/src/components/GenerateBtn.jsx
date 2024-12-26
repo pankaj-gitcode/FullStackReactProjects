ec7a72a66a09fe598b99f53bca81ebf11975677e
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-26 17:07:33 +0530

    GenerateBtn_component_v1

--- textToImageGenerator_webApp/src/components/GenerateBtn.jsx
+++ textToImageGenerator_webApp/src/components/GenerateBtn.jsx
@@ -6,14 +6,15 @@ export default function GenerateBtn(){
         <div className='flex items-center justify-center py-12'>
             <div className='flex flex-col flex-wrap items-center justify-center'>
                 
-                <h1 className='text-3xl sm:text-3xl md:text-5xl pb-6'>See the magix. Try now</h1>
+                <h1 className='text-3xl sm:text-3xl md:text-[5vw] lg:text-5xl pb-6'>See the magic. Try now</h1>
 
                 <button className='flex items-center justify-center 
-                bg-black text-white px-10 py-3 rounded-full'>
+                bg-black text-white px-10 py-3 rounded-full
+                active:scale-105 duration-300 ease-in-out'>
 
-                    <p className='text-2xl'>Generate Images</p>
+                    <p className='text-xl sm:text-lg lg:text-sm'>Generate Images</p>
                     <img src={assets.star_group} alt={assets.star_group} 
-                        className='w-16'
+                        className='w-6'
                     />
                 </button>
             </div>
