cb54b2845eadd87125cf67f38889cd638ffc8d71
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-26 11:51:36 +0530

    Introduction_page- final vrsn

--- textToImageGenerator_webApp/src/components/Testimonials.jsx
+++ textToImageGenerator_webApp/src/components/Testimonials.jsx
@@ -3,33 +3,43 @@ import { assets, testimonialsData } from '../assets/assets'
 
 export default function Testimonials() {
   return (
-    <div className='min-h-[100vh] pt-5'>
-        {/* ---------- TITLE ---------- */}
-        <div className='flex flex-col flex-wrap-reverse items-center justify-center py-3'>
-            <h1 className='text-xl sm:text-3xl lg:text-5xl'>Customer testimonials</h1>
-            <p className='text-xs sm:text-lg text-gray-500'>What Our Users Are Saying</p>
-        </div>
-        {/* ---------- CARDS ---------- */}
-        <div className='flex items-center justify-center gap-3 py-6'>
-            {
-                testimonialsData.map((elem,i)=>
-                <div key={i} className='bg-[#fff] flex flex-wrap flex-col items-center justify-center py-16 px-8 rounded-xl
-                gap-2 border-2 border-[#E1E1E1]'>
-                    <img src={elem.image} alt={elem.image} className='w-[50vw] sm:w-[10vw] lg:w-[5vw]]' />
-                    <h1 className='text-[8vw] sm:text-[3.8vw] md:text-[2.5vw] lg:text-[1.5vw]'>{elem.name}</h1>
-                    <p className='text-2xl md:text-[1vw]'>{elem.role}</p>
-                    <div className='flex items-center gap-1'>
-                        {
-                        Array(elem.stars).fill().map((ele,i)=>
-                            <img key={i} src={assets.rating_star} alt={assets.rating_star} />
-                        )  
-                        }
+    <div className='pb-10'>
+
+            {/* ------------- TITLE ------------ */}
+            <div className='flex flex-col items-center justify-center gap-2 pb-12'>
+                <h1 className='text-3xl sm:text-4xl lg:text-5xl text-center'>Customer testimonials</h1>
+                <p className=' text-xl sm:text-xl text-center text-stone-500'>What Our Users Are Saying</p>
+            </div>
+            {/* ------------- CARD ------------ */}
+
+            <div className='flex sm:flex-row flex-col items-center justify-center gap-3'>
+                {
+                    testimonialsData.map((elem,i)=>
+                    <div key={i} className='flex flex-col items-center justify-center
+                    border-2 border-[#E1E1E1] bg-white rounded-xl shadow-[0px_2px_20px_0px_rgba(0,0,0,0.2)]
+                     px-2 text-center gap-1  h-[50vh] sm:w-[30vw] lg:w-[20vw]
+                     cursor-pointer hover:scale-105 duration:300 transition ease-in-out '>
+
+                        <img src={elem.image} alt={elem.image} />
+                        <h1 className='text-xl font-semibold'>{elem.name}</h1>
+                        <p className='text-sm text-stone-600'>{elem.role}</p>
+                        
+                        <div className='flex py-2'>
+                            {
+                                Array(elem.stars).fill().map((ele,i)=>
+                                <img key={i} src={assets.rating_star} alt={assets.rating_star} /> )
+                            }
+                        </div>
+
+                        <p className='sm:text-[2vw] md:text-[1.8vw] lg:text-[1vw] text-stone-700 leading-6'>
+                        I&apos;ve been using imagify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.
+                        </p>
+
                     </div>
-                    <p className='text-center'>I&apos;ve been using imagify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier</p>
-                </div>
-                )
-            }
-        </div>
+                    )
+                }
+            </div>
+
     </div>
   )
 }
