2a891de29195e2e8023069e080aa5771c14060fc
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-28 22:26:29 +0530

    Result_v1

--- textToImageGenerator_webApp/src/pages/Result.jsx
+++ textToImageGenerator_webApp/src/pages/Result.jsx
@@ -11,12 +11,13 @@ export default function Result() {
         />
         <span className='absolute h-1 min-w-full bg-blue-500 left-0 bottom-0'/>
       </div>
-        <p className='bg-red-500'>Loading...</p>
+        <p className='text-[6vw] sm:text-[2vw] md:text-[3.8vw] lg:text-[1.5vw]'>Loading...</p>
 
         {/* ---------- INPUT, BUTTON ------- */}
-      <div>
-        <input type="text" placeholder='Describe what you want to generate' />
-        <button>Generate</button>
+      <div className='bg-neutral-500 flex flex-col sm:flex-row text-white rounded-full'>
+        <input type="text" placeholder='Describe what you want to generate'
+        className='flex-1 bg-transparent w-96 max-sm:w-30 pl-6 pr-16 py-1 text-lg outline-none' />
+        <button className='bg-zinc-900 rounded-full text-xl sm:text-sm px-5'>Generate</button>
       </div>
 
       {/* --------- DOWNLOAD -------- */}
