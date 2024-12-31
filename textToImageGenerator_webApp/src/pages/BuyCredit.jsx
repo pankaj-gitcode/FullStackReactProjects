5de40ad360b705c94907f4536a4ed6411badd0a0
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-31 10:07:58 +0530

    BuyCredit__v1

--- textToImageGenerator_webApp/src/pages/BuyCredit.jsx
+++ textToImageGenerator_webApp/src/pages/BuyCredit.jsx
@@ -6,17 +6,23 @@ export default function BuyCredit() {
     <div className='min-h-screen flex justify-center'>
       <div className='flex flex-col items-center gap-3'>
         <p className='uppercase border-2 rounded-full bg-white text-center text-sm px-6 py-2  '>Our Plans</p>
-        <h1 className='text-xl sm:text-2xl md:text-3xl mb-5'>Choose the plan</h1>
+        <h1 className='text-[12vw] sm:text-5xl md:text-3xl mb-5'>Choose the plan</h1>
         {/* ------------- CARDS ---------- */}
-        <div className=''>
+        <div className='flex lg:flex-row flex-col items-center justify-center gap-6'>
           {
             plans.map((elem,i)=>
-            <div key={i}>
-              <h1>{elem.id}</h1>
-              <p>{elem.desc}</p>
-              <span>${elem.price}/{elem.credits}</span>
+            <div key={i} 
+            className='flex flex-col items-center  bg-white
+            border-[#ededed] border-2 h-[60vh] w-[90vw] sm:w-[50vw] lg:w-[25vw] rounded-xl  text-center'>
+              
+              <h1 className='pt-[10vh] text-3xl font-semibold'>{elem.id}</h1>
+              <p className='text-xl sm:text-sm text-[#515151] py-3'>{elem.desc}</p>
+              <p className='pt-5 pb-3 text-[12vw] sm:text-2xl md:text-3xl font-semibold'>${elem.price}/
+                  <span className='text-sm text-[#515151]'>{elem.credits} credits</span>
+              </p>
 
-              <button>Get Started</button>
+              <button className='bg-black text-white rounded-lg 
+              md:px-12 py-3 px-6 text-lg md:text-[1vw] mt-10'>Get Started</button>
             </div>
             )
           }
