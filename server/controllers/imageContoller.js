a8980e8fdf0cc3a9391c29e966d6f6a6d174fa1b
pankaj-gitcode <pankajaditya75@gmail.com>
2025-01-17 22:46:41 +0530

    imageController  v1

--- server/controllers/imageContoller.js
+++ server/controllers/imageContoller.js
@@ -2,4 +2,17 @@ import userModel from "../models/userModel";
 
 export const imageController= async(req,res)=>{
     const {userId, prompt} = req.body;
+
+    // find userId in DB
+    const user = await userModel.findById(userId);
+
+    // if data missing
+    if(!userId || !prompt){
+        return res.status(400).json({success:false, message: 'Mssing Data...'});
+    }
+
+    // no credit
+    if(user.creditBalance === 0 || userModel.createBalance < 0){
+        return res.json(404).json({success: false, message:'No Credit...'})
+    }
 }
