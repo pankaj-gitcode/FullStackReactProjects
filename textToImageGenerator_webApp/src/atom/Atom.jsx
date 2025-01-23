7ff3267483865d4fcf960c1fcde2ed3e1c6ba25b
pankaj-gitcode <pankajaditya75@gmail.com>
2025-01-23 22:12:55 +0530

    backend URL token code updated

--- textToImageGenerator_webApp/src/atom/Atom.jsx
+++ textToImageGenerator_webApp/src/atom/Atom.jsx
@@ -15,3 +15,14 @@ export const exitAtom = atom({
     key: 'exitAtom',
     default: 0
 })
+
+//  ------ BACKEND TO FRONTEBD ---------
+export const backendUrlAtom = atom({
+    key: 'backendUrlAtom',
+    default: import.meta.env.VITE_BACKEND_URL
+})
+
+export const tokenAtom = atom({
+    key: 'tokenAtom',
+    default: localStorage.getItem('token')
+})
